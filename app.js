736ef37654078741ee0769f5df50283e4bd343ca
EdwardCullen30 <f20180872@pilani.bits-pilani.ac.in>
2020-04-04 14:25:45 +0530

    initial

---
+++ app.js
@@ -0,0 +1,82 @@
+const express = require('express');
+const expressLayouts = require('express-ejs-layouts');
+const ejs = require('ejs');
+const path = require('path');
+const mongoose = require('mongoose');
+const flash = require('connect-flash');
+const session = require('express-session');
+const passport = require('passport');
+
+
+
+// Passport Config
+require('./config/passport')(passport);
+
+
+// Init app
+const app = express();
+
+
+
+
+
+
+//connecting mongodb link
+const MONGODB_URL ='mongodb+srv://EdwardCullen30:@S30n08y00@zwinger1-z4zgy.mongodb.net/test?retryWrites=true&w=majority';
+//connecting
+mongoose.connect(MONGODB_URL,  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+mongoose.connection.on('connected', () => {
+    console.log('Mongoose is connected!!!!');
+});
+
+
+//Public folder
+app.use(express.static('public'));
+
+
+// Express session
+app.use(
+  session({
+    secret: 'secret',
+    resave: true,
+    saveUninitialized: true
+  })
+);
+// Passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
+// Connect flash
+app.use(flash());
+// Global variables
+app.use(function(req, res, next) {
+  res.locals.success_msg = req.flash('success_msg');
+  res.locals.error_msg = req.flash('error_msg');
+  res.locals.error = req.flash('error');
+  next();
+});
+
+// EJS
+app.set('view engine', 'ejs');
+
+
+// Data parsing
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+//static
+app.use(express.static('public'));
+
+
+//setting routes
+app.use('/',require('./routes/index'));
+app.use('/users',require('./routes/users'));
+
+
+
+//starting connection
+const port = 3000;
+app.listen(port, () => console.log(`Server started on port ${port}`));
---
+++ config/mailerauth.js
@@ -0,0 +1,4 @@
+module.exports = {
+  MAILGUN_USER: 'postmaster@sandbox5b8f28ef167747129fa1ddd2f1bf1c4e.mailgun.org',
+  MAILGUN_PASS: 'd6327b29b16e89402472255bf0fcd15c-46ac6b00-d274bc1d'
+};
